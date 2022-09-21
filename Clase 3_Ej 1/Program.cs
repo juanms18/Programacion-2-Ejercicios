dba3e57d53ee2eda3888e70444948666b3b2c56c
juanms18 <manuel_lp_18@hotmail.com>
2022-09-20 22:50:49 -0300

    segundo commit de hoy

--- Clase 3_Ej 1/Program.cs
+++ Clase 3_Ej 1/Program.cs
@@ -11,24 +11,24 @@ namespace Clase_3_Ej_1
            
 
 
-            Console.WriteLine($"Datos de la cuenta por defecto:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
-            Console.WriteLine("");
+            //Console.WriteLine($"Datos de la cuenta por defecto:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
+            //Console.WriteLine("");
 
-            Console.WriteLine("Ingreso 21.99 pesos: ");
-            cuenta1.Ingresar(21.99M);
-            Console.WriteLine($"Datos de la cuenta luego de ingresar dinero:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
-            Console.WriteLine("");
+            //Console.WriteLine("Ingreso 21.99 pesos: ");
+            //cuenta1.Ingresar(21.99M);
+            //Console.WriteLine($"Datos de la cuenta luego de ingresar dinero:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
+            //Console.WriteLine("");
 
-            Console.WriteLine($"voy a mostrar probando \n{cuenta1.Mostrar()}");
+            //Console.WriteLine($"voy a mostrar probando \n{cuenta1.Mostrar()}");
 
-            Console.WriteLine("Retiro 800 pesos: ");
-            cuenta1.Retirar(800);
-            Console.WriteLine($"Datos de la cuenta luego de retirar dinero:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
-            Console.WriteLine("");
+            //Console.WriteLine("Retiro 800 pesos: ");
+            //cuenta1.Retirar(800);
+            //Console.WriteLine($"Datos de la cuenta luego de retirar dinero:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
+            //Console.WriteLine("");
 
-            Console.WriteLine("Retiro 600 pesos: ");
-            cuenta1.Retirar(800);
-            Console.WriteLine($"Datos de la cuenta luego de retirar dinero:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
+            //Console.WriteLine("Retiro 600 pesos: ");
+            //cuenta1.Retirar(800);
+            //Console.WriteLine($"Datos de la cuenta luego de retirar dinero:\nTitular: {cuenta1.Titular()}\nMonto: {cuenta1.Cantidad()}");
 
 
             Console.WriteLine("La cantida de cuentas es: {0}", Cuenta.ContadorDeCuentas) ;
