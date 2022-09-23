28f95c519258d0e618c20914fd7908da69567359
juanms18 <manuel_lp_18@hotmail.com>
2022-09-22 23:54:01 -0300

    Ejercicio 2 clase 3

---
+++ Clase 3_ej 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using PersonaEj2;
+
+
+namespace Clase_3_ej_2
+
+	
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Juan Perez", new DateTime(1970, 09, 01), 20159751);
+            Persona persona2 = new Persona("Alejo Carmona", new DateTime(1999, 09, 20), 42146540);
+            Persona persona3 = new Persona("Martina Gomez", new DateTime(2005, 04, 29), 43015237);
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Persona 1:");
+            Console.WriteLine(persona1.Mostrar());
+            Console.WriteLine(persona1.EsMayorDeEdad());
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Persona 2:");
+            Console.WriteLine(persona2.Mostrar());
+            Console.WriteLine(persona2.EsMayorDeEdad());
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Persona 3:");
+            Console.WriteLine(persona3.Mostrar());
+            Console.WriteLine(persona3.EsMayorDeEdad());
+        }
+    }
+}
---
+++ Persona/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace Persona
+{
+    public class Persona
+    {
+        //ATRIBUTOS:
+        private string nombre;
+        private DateTime fechaDeNacimiento;
+        private int dni;
+
+        //CONSTRUCTOR:
+        public Persona()
+        {
+            nombre = "Persona";
+            fechaDeNacimiento = DateTime.Now.Date;
+            dni = 44000000;
+        }
+
+        public Persona(string nombre, DateTime fechaDeNacimiento, int dni)
+        {
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento.Date;
+            this.dni = dni;
+        }
+
+        //GETTERS:
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+
+        public DateTime GetFechaDeNacimiento()
+        {
+            return this.fechaDeNacimiento;
+        }
+
+        public int GetDni()
+        {
+            return this.dni;
+        }
+
+        //METODOS:
+        private int CalcularEdad()
+        {
+            //TODO: mejorar metodo ; se puede usar TimeSpan
+            int edadActual = 0;
+            DateTime fechaActual = DateTime.Now.Date;
+            DateTime nacimientoPersona = this.fechaDeNacimiento;
+
+            while (nacimientoPersona.Year < fechaActual.Year)
+            {
+                nacimientoPersona = nacimientoPersona.AddYears(1);
+                if (nacimientoPersona.Year != fechaActual.Year)
+                    edadActual++;
+            }
+
+            if (DateTime.Compare(fechaActual, nacimientoPersona) > 0 || DateTime.Compare(fechaActual, nacimientoPersona) == 0)
+            {
+                edadActual++;
+            }
+
+            return edadActual;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Nombre: {this.nombre}");
+            sb.AppendLine($"Fecha de nacimiento: {this.fechaDeNacimiento}");
+            sb.AppendLine($"Edad: {this.CalcularEdad()}");
+            sb.Append($"DNI: {this.dni}");
+
+            return sb.ToString();
+        }
+
+        public string EsMayorDeEdad()
+        {
+            string mensaje = "Es mayor de edad";
+
+            if (this.CalcularEdad() < 18)
+            {
+                mensaje = "No es mayor de edad";
+            }
+
+            return mensaje;
+        }
+    }
+}
